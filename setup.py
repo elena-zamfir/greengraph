23e13eed77e476671db639937f25d824fec8d975
elena zamfir <elenazamfir@pc-209-248.wireless.lon4.opal.lan>
2015-11-27 02:08:15 +0000

    Comment added about what should already work and what not

--- setup.py
+++ setup.py
@@ -6,3 +6,5 @@ packages = find_packages(exclude = ['*test']),
 #scripts = ['scripts/plot_green_between'],
 install_requires = ['argparse']
 )
+# I should now be able to install the package and import it wherever. But I have no script
+#yet to be executed as command line commands.
