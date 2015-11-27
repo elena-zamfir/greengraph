84b7e25d102f2cac580f72d54f8540893b1fb462
elena zamfir <elenazamfir@pc-209-248.wireless.lon4.opal.lan>
2015-11-27 02:04:22 +0000

    First version of setup.py

---
+++ greengraph/command.py
@@ -0,0 +1,20 @@
+from argparse import ArgumentParser
+from Greengraph import  Map
+from Greengraph import Greengraph
+import matplotlib.pyplot as plt
+
+
+def process():
+    parser = ArgumentParser(description = "Generate graph of green spaces between tho given locations")
+
+
+    parser.add_argument('start')
+    parser.add_argument('finish')
+    parser.add_argument('nr_steps')
+    arguments = parser.parse_args()
+
+    plt.plot(Greengraph(arguments.start ,arguments.finish).green_between(arguments.nr_steps))
+    plt.show()
+
+if __name__ == "__main__":
+    process()
