a5984b4dfbcee7ddea886caa0b6c7c4d3da94051
elena zamfir <elenazamfir@elenas-MacBook-Air.local>
2015-12-25 23:44:12 +0000

    adding default values of arguments with default keyword

--- greengraph/command.py
+++ greengraph/command.py
@@ -8,24 +8,13 @@ def process():
     parser = ArgumentParser(description = "Generate graph of green spaces between tho given locations")
 
 
-    parser.add_argument('--start', help="starting point" )
-    parser.add_argument('--finish', help = 'end point')
-    parser.add_argument('--nr_steps', help = 'nr points in between', type=int )
+    parser.add_argument('--start', help="starting point" , default = 'London')
+    parser.add_argument('--finish', help = 'end point', default = 'Oxford')
+    parser.add_argument('--nr_steps', help = 'nr points in between', type=int, default = 10 )
     arguments = parser.parse_args()
-    if arguments.start:
-        start = arguments.start;
-    else:
-        start = 'London';
-    if arguments.finish:
-        finish = arguments.finish;
-    else:
-        finish  = 'Oxford';
-    if arguments.nr_steps:
-        nr_steps = arguments.nr_steps;
-    else:
-        nr_steps = 10;
+
     plt.close()
-    plt.plot(Greengraph(start ,finish).green_between(nr_steps))
+    plt.plot(Greengraph(arguments.start ,arguments.finish).green_between(arguments.nr_steps))
     plt.show()
 
 if __name__ == "__main__":
