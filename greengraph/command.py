7907ecfac406231f593fe7471733b37522666620
elena zamfir <elenazamfir@elenas-MacBook-Air.local>
2015-12-25 23:33:31 +0000

    remake all arguments optional and provide default values for them

--- greengraph/command.py
+++ greengraph/command.py
@@ -8,13 +8,24 @@ def process():
     parser = ArgumentParser(description = "Generate graph of green spaces between tho given locations")
 
 
-    parser.add_argument('start', help="starting point",
-                    type=str )
-    parser.add_argument('finish', help = 'end point', type = str)
-    parser.add_argument('nr_steps', help = 'nr points in between', type=int )
+    parser.add_argument('--start', help="starting point" )
+    parser.add_argument('--finish', help = 'end point')
+    parser.add_argument('--nr_steps', help = 'nr points in between', type=int )
     arguments = parser.parse_args()
-
-    plt.plot(Greengraph(arguments.start ,arguments.finish).green_between(arguments.nr_steps))
+    if arguments.start:
+        start = arguments.start;
+    else:
+        start = 'London';
+    if arguments.finish:
+        finish = arguments.finish;
+    else:
+        finish  = 'Oxford';
+    if arguments.nr_steps:
+        nr_steps = arguments.nr_steps;
+    else:
+        nr_steps = 10;
+    plt.close()
+    plt.plot(Greengraph(start ,finish).green_between(nr_steps))
     plt.show()
 
 if __name__ == "__main__":
