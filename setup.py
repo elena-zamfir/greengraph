50c4ef25feb39bd3eacfa31ccd1b9f7ab8e8fffa
elena zamfir <elenazamfir@elenas-MacBook-Air.local>
2015-12-27 08:52:35 +0000

    comments changed accordingly

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ setup(
 name = "Greengraph",
 version = "0.1",
 packages = find_packages(exclude = ['*test']),
-scripts = ['scripts/plot_green_between'],
+scripts = ['scripts/plot_green_between'], #now command line call to plot_green_between works
 install_requires = ['argparse']
 )
-# I should now be able to install the package and import it wherever. But I have no script
-#yet to be executed as command line commands.
+ 
