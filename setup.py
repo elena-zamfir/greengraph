a862ca6440cc3a9d5f99be089e1cb56c71a28116
elena zamfir <elenazamfir@pc-209-248.wireless.lon4.opal.lan>
2016-01-10 11:46:02 +0000

    changed name of script to greengraph

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ setup(
 name = "Greengraph",
 version = "0.1",
 packages = find_packages(exclude = ['*test']),
-scripts = ['scripts/plot_green_between'], #now command line call to plot_green_between works
+scripts = ['scripts/greengraph'], #now command line call to plot_green_between works
 install_requires = ['argparse']
 )
- 
